2f36e9d4d43f0a97def46ec4d81a59720b67eb8e
Angga <felangga@gmail.com>
2022-08-04 01:32:03 +0700

    add db query

---
+++ db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.products (
+    id uuid NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    name character varying NOT NULL,
+    original_price numeric DEFAULT 0,
+    price numeric DEFAULT 0 NOT NULL,
+    rating double precision DEFAULT 0,
+    rating_average double precision DEFAULT 0,
+    url text,
+    url_img text
+);
